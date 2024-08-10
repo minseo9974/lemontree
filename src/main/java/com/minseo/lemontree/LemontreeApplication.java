6f6a889af054db46677e923ce7751661e4366c57
minseo <sa4777@naver.com>
2024-08-10 11:59:00 +0900

    project commit

---
+++ src/main/java/com/minseo/lemontree/LemontreeApplication.java
@@ -0,0 +1,13 @@
+package com.minseo.lemontree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemontreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemontreeApplication.class, args);
+	}
+
+}
