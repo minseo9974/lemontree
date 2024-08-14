bfefacf4d37b2c8161ebb6480e2ee6b6339b9c2e
minseo <sa4777@naver.com>
2024-08-15 04:17:15 +0900

    [ADD] H2 properties 추가

--- src/test/java/com/minseo/lemontree/ConcurrencyTest.java
+++ src/test/java/com/minseo/lemontree/ConcurrencyTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 
 /**
@@ -32,6 +33,7 @@ import org.springframework.test.util.ReflectionTestUtils;
  */
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ConcurrencyTest {
     @Autowired
     private PaymentService paymentService;
